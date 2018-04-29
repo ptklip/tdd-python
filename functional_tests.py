e20a8771d217852baaf9eb6739a1dd54d23c6b1f
Peter Klipstein <ptklip@gmail.com>
2018-04-29 08:43:42 -0400

    Now using unittest.

--- functional_tests.py
+++ functional_tests.py
@@ -1,7 +1,21 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
 
-assert 'Django' in browser.title
+	def setUp(self):
+		self.browser = webdriver.Firefox()
 
+	def tearDown(self):
+		self.browser.quit()
+
+	def test_can_start_a_list_and_retrieve_it_later(self):
+		# User checks the homepageself.
+		self.browser.get('http://localhost:8000')
+
+		# Page title is 'To-Do'
+		self.assertIn('To-Do', self.browser.title)
+		self.fail('Finish the test!')
+
+if __name__ == '__main__':
+	unittest.main()
