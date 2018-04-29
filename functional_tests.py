d798c8dbd020ed291b5e1f8570d7bfd534ebe1aa
Peter Klipstein <ptklip@gmail.com>
2018-04-29 18:15:32 -0400

    Functional test now checks we can input a to-do item.

--- functional_tests.py
+++ functional_tests.py
@@ -10,12 +10,41 @@ class NewVisitorTest(unittest.TestCase):
 		self.browser.quit()
 
 	def test_can_start_a_list_and_retrieve_it_later(self):
-		# User checks the homepageself.
+		# Wallace checks the homepage.
 		self.browser.get('http://localhost:8000')
 
-		# Page title is 'To-Do'
+		# Wallace sees that the page title and header mention to-do lists
 		self.assertIn('To-Do', self.browser.title)
+		header_text = self.browser.find_element_by_tag_name('h1').text
+		self.assertIn('To-Do', header_text)
+
+		# Wallace is invited to enter a to-do item immediately.
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertEqual(
+			inputbox.get_attribute('placeholder'),
+			'Enter a to-do item'
+		)
+
+		# Wallace types "Buy cheese" into a text box.
+		inputbox.send_keys('Buy cheese')
+
+		# When Wallace hits enter, the page updates to show
+		# "1. Buy cheese" as a to-do list item
+		inputbox.send_keys(Keys.ENTER)
+		time.sleep(1)
+
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertTrue(
+			any(row.text == '1: Buy cheese' for row in rows)
+		)
+
+		# There is stil la text box inviting Wallace to enter another item.
+		# Wallace enters "Buy crackers"
 		self.fail('Finish the test!')
+
+		# The page updates and now shows two items in the list.
+
 
 if __name__ == '__main__':
 	unittest.main()
